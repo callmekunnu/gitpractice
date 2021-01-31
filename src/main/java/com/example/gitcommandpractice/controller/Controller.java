1b61067f153d8eb93350256c16fa937dc151d331
Kunal A <kunal_aggarwal789@yahoo.com>
2021-01-31 15:24:28 +0530

    initial commit

---
+++ src/main/java/com/example/gitcommandpractice/controller/Controller.java
@@ -0,0 +1,15 @@
+package com.example.gitcommandpractice.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Controller {
+	
+	public String greetMsg()
+	{
+		return "Hello GitHub";
+		
+	}
+	
+
+}
