da04488eff56da885e04c105afb6ab8527d96a2a
Kunal A <kunal_aggarwal789@yahoo.com>
2021-01-31 16:00:17 +0530

    greet msg is added

--- src/main/java/com/example/gitcommandpractice/controller/Controller.java
+++ src/main/java/com/example/gitcommandpractice/controller/Controller.java
@@ -1,17 +1,24 @@
 package com.example.gitcommandpractice.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@RequestMapping("/api")
 @RestController
 public class Controller {
-
-private int i=10;
 	
+	@GetMapping("/greet")
 	public String greetMsg()
 	{
 		return "Hello GitHub";
-		
 	}
 	
+	@GetMapping("/greet2")
+	public String greetMsg2()
+	{
+		return "Good Mrng GitHub and hello stash";
+		
+	}
 
 }
